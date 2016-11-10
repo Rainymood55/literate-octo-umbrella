c2a96e79240f2f6bd520efe8466b8c3cd32a0a34
Rainymood55 <comprehend_cy@126.com>
2016-11-10 17:08:47 +0800

    Update NewtonRapForSquare.py

--- NewtonRapForSquare.py
+++ NewtonRapForSquare.py
@@ -1,4 +1,14 @@
 # Newton-Raphson for square root
+''' 
+P(g) = an*x^n + an-1*x^n-1 + ... +a1*x + a0
+Want to find r such that p(r)=0
+For example, to find the square root of 24,find the root of P(x)=x^-24
+Now, what Newton showed was that for things like
+polynomials, if g is an approximation to the root, the
+place where it's equal to 0, then g - p(g) / p'(g) is a
+better approximation, where p' is just the derivative of p.
+And if you haven't done calculus,
+'''
 
 epsilon = 0.01
 y = 24.0
